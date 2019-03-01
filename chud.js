fb906ee040e7d5806b2cc19fd2832daa3841922e
peterbatridge <11344363+peterbatridge@users.noreply.github.com>
2019-02-28 21:34:09 -0600

    smaller canvas

--- chud.js
+++ chud.js
@@ -46,8 +46,8 @@ var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
         document.onkeydown = function (e) { keyHandler(e); };
-        this.canvas.width = window.innerWidth;
-        this.canvas.height = window.innerHeight;
+        this.canvas.width = window.innerWidth - 100;
+        this.canvas.height = window.innerHeight- 100;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
