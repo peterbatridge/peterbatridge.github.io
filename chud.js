45fea66750a9f999399894f184a3a7be06ebe187
peterbatridge <11344363+peterbatridge@users.noreply.github.com>
2019-02-28 18:36:57 -0600

    a

--- chud.js
+++ chud.js
@@ -4,7 +4,7 @@ var myObstacles = [];
 var myScore;
 
 function startGame() {
-    myGamePiece = new component(30, 30, "white", 10, 120);
+    myGamePiece = new component(30, 30, "red", 10, 120);
     myGamePiece.gravity = 0.05;
     myScore = new component("30px", "Consolas", "black", 280, 40, "text");
     myGameArea.start();
@@ -13,8 +13,8 @@ function startGame() {
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 780;
-        this.canvas.height = 540;
+        this.canvas.width = 480;
+        this.canvas.height = 270;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
