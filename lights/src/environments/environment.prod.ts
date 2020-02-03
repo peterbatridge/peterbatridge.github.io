ea4a56b2975dd46f1527008a5372469a04a615ea
peterbatridge <11344363+peterbatridge@users.noreply.github.com>
2020-02-03 01:08:09 -0600

    do it

--- lights/src/environments/environment.prod.ts
+++ lights/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBDQhpbONtHCGv7v0v8MJtaXx6WUbmTM6Q",
+    authDomain: "nonagons-d7a33.firebaseapp.com",
+    databaseURL: "https://nonagons-d7a33.firebaseio.com",
+    projectId: "nonagons-d7a33",
+    storageBucket: "nonagons-d7a33.appspot.com",
+    messagingSenderId: "226203451109",
+    appId: "1:226203451109:web:7a74adbf5e55fa2decde0c",
+    measurementId: "G-8RNYC55DM7"
+  }
 };
