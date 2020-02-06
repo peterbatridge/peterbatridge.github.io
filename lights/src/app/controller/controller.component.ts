103818e0c188f19f70d15cd6ff130f01a801e209
peterbatridge <11344363+peterbatridge@users.noreply.github.com>
2020-02-05 23:48:39 -0600

    changing the way function dropdowns work

--- lights/src/app/controller/controller.component.ts
+++ lights/src/app/controller/controller.component.ts
@@ -4,6 +4,7 @@ import {FirebaseUISignInSuccessWithAuthResult} from 'firebaseui-angular';
 import { Router } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { range } from 'rxjs';
+import { stringify } from 'querystring';
 
 @Component({
   selector: 'controller',
@@ -107,19 +108,43 @@ setupNumberField(arg, selected="") {
         };
     this.selectsList.push(dropdown);
   }
-
-setupSelectField(arg, options, selected="") {
+customArrayToString(arr: Array<any>, nested="") {
+  let str = "[";
+  for (let i = 0; i < arr.length; i ++) {
+    if (Array.isArray(arr[i])) { 
+      let n = "";
+      if (i < arr.length-1) {
+        n = ", "
+      }
+      str+= this.customArrayToString(arr[i], n);
+    }
+    else {
+      str += arr[i];
+      if (i < arr.length-1) {
+        str += ", ";
+      }
+    }
+  }
+  return str+"]"+nested;
+}
+setupSelectField(arg, options, selected="", isAFunction=false) {
       let dropdown = {
         name: arg['name'],
         selected: selected,
         notes: arg['notes'],
-        type: 'SELECT',
+        type: isAFunction ? 'FUNCTION' : 'SELECT',
         options: []
         };
+        console.log(options);
         for (let i in options) {
-          dropdown.options.push({name: i, value:options[i]})
-          if (arg.type == "direction") {
+          if (isAFunction) {
+            dropdown.options.push({name: options[i].valueName, value:options[i].value});
+          }
+          else if (arg.type == "direction") {
               dropdown.options.push({name: options[i], value:"'"+options[i]+"'"})
+          }
+          else {
+            dropdown.options.push({name: i, value:options[i]})
           }
         }
     this.selectsList.push(dropdown);
@@ -128,12 +153,29 @@ setupSelectField(arg, options, selected="") {
 clearArgumentFields() {
       console.log("clearing fields");
       this.selectsList = [];
+}
+
+getKeyByValue(object, value) {
+  return Object.keys(object).find(key => object[key].functionName === value);
+}
+
+addFunctionSelection() {
+  this.setupSelectField({ name: "Select a function", notes: ""}, this.functionsList, "", true);
+}
+
+selectionChange() {
+  for (let i = 0; i < this.selectsList.length; i++) {
+    if (this.selectsList[i].type == 'FUNCTION') {
+      console.log(this.selectsList[i]);
+    }
   }
+}
 selectState() {
     this.clearArgumentFields();
     for (let j in this.states[this.stateSelection].functions) {
         var selection = this.functions[this.states[this.stateSelection].functions[j]];
         console.log(selection);
+        this.setupSelectField({ name: "Select a function", notes: ""}, this.functionsList, this.getKeyByValue(this.functions, selection.functionName), true);
         let args = JSON.parse(this.states[this.stateSelection].args[j]);
         console.log(args);
         for (let i in selection.args) {
@@ -143,87 +185,57 @@ selectState() {
                 this.setupNumberField(selection.args[i], args[i]);
                 break;
                 case "color":
-                this.setupSelectField(selection.args[i],this.colors, args[i]);
+                this.setupSelectField(selection.args[i],this.colors, this.customArrayToString(args[i]));
                 break;
                 case "colorSequence":
-                this.setupSelectField(selection.args[i],this.colorSequences,args[i]);
+                this.setupSelectField(selection.args[i],this.colorSequences, this.customArrayToString(args[i]));
                 break;
                 case "groupOfNonagons":
-                this.setupSelectField(selection.args[i],this.groupsOfNonagons,args[i]);
+                this.setupSelectField(selection.args[i],this.groupsOfNonagons, this.customArrayToString(args[i]));
                 break;
                 case "setOfGroupsOfNonagons":
-                this.setupSelectField(selection.args[i],this.setsOfGroupsOfNonagons,args[i]);
+                this.setupSelectField(selection.args[i],this.setsOfGroupsOfNonagons, this.customArrayToString(args[i]));
                 break;
                 case "audioMappings":
-                this.setupSelectField(selection.args[i],this.audioMappings,args[i]);
+                this.setupSelectField(selection.args[i],this.audioMappings, this.customArrayToString(args[i]));
                 break;
                 case "direction":
-                this.setupSelectField(selection.args[i],this.directions,args[i]);
+                this.setupSelectField(selection.args[i],this.directions, this.customArrayToString(args[i]));
                 break;
             }
         }
     }
     console.log(this.selectsList);
 }
-showArgumentChoices() {
-      this.clearArgumentFields();
-      var selection = this.functions[this.functionSelection];
-      for (let i in selection.args) {
-          console.log(selection.args[i].type)
-          switch(selection.args[i].type) {
-              case "number":
-                this.setupNumberField(selection.args[i]);
-                break;
-              case "color":
-                this.setupSelectField(selection.args[i],this.colors);
-                break;
-              case "colorSequence":
-                this.setupSelectField(selection.args[i],this.colorSequences);
-                break;
-              case "groupOfNonagons":
-                this.setupSelectField(selection.args[i],this.groupsOfNonagons);
-                break;
-              case "setOfGroupsOfNonagons":
-                this.setupSelectField(selection.args[i],this.setsOfGroupsOfNonagons);
-                break;
-              case "audioMappings":
-                this.setupSelectField(selection.args[i],this.audioMappings);
-                break;
-              case "direction":
-                this.setupSelectField(selection.args[i],this.directions);
-                break;
-          }
-      }
-      console.log(this.selectsList);
-  }
+
 setupFunctionSelect() {
-      for (let index in this.functions) {
-        var f = this.functions[index];
-        this.functionsList.push(
-            {
-                value: index,
-                valueName: f.functionName
-            }
-        );
+    for (let index in this.functions) {
+      var f = this.functions[index];
+      this.functionsList.push(
+          {
+              value: index,
+              valueName: f.functionName
+          }
+      );
+    }
+    console.log(this.functionsList);
+}
+
+ngOnInit(): void {
+  this.afAuth.authState.subscribe(d => {
+      console.log(d);
+      if (d == null) {
+          this.router.navigateByUrl("");
       }
-      console.log(this.functionsList);
-  }
+  });
+}
 
-  ngOnInit(): void {
-    this.afAuth.authState.subscribe(d => {
-        console.log(d);
-        if (d == null) {
-            this.router.navigateByUrl("");
-        }
-    });
-  }
+logout() {
+    console.log("logout? p2");
+  this.afAuth.auth.signOut();
+}
 
-  logout() {
-      console.log("logout? p2");
-    this.afAuth.auth.signOut();
-  }
-
-  successCallback(data: FirebaseUISignInSuccessWithAuthResult) {
-    console.log(data);
-  }
+successCallback(data: FirebaseUISignInSuccessWithAuthResult) {
+  console.log(data);
+}
 }
